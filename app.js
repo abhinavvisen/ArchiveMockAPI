a27d80243338be9886ae64545ca11d23b5a41500
Abhinavvisen <visenabhinav96@gmail.com>
2017-01-01 20:29:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,20 +5,22 @@ var app = express();
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-mongoose.connect('mongodb://test:test@ds145315.mlab.com:45315/api');
+//Connecting to database
+mongoose.connect('mongodb://test2:test2@ds013366.mlab.com:13366/api');
 app.set('view engine','ejs');
 app.use('/assets',express.static('assets'));
 
-var schema =new mongoose.Schema({
-  classNumber:Number,
-  RegistrationNumber:String
+var Schema = new mongoose.schema({
+  classNumber: Number,
+  registrationNumber:String
 });
-
-
-
-
 
 app.get('/login',function(req,res){
   res.render('index');
+  console.log();
+});
+app.post('/login',urlencodedParser,function(req,res){
+  console.log(req.body);
+
 });
 app.listen(3000);
